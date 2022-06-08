70c1e316861352a215b08ec6c6e18726939b3c4d
Safiullah Omar <omar.safiullah@gmail.com>
2022-06-08 11:12:23 +0430

    Angular project rout and apis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDeptComponent } from './department/show-dept/show-dept.component';
+import { AddEditDeptComponent } from './department/add-edit-dept/add-edit-dept.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
+import {SharedServiceService} from './shared-service.service';
+import{ HttpClientModule} from '@angular/common/http';
+import{ ReactiveFormsModule,FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ShowDeptComponent,
+    AddEditDeptComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [SharedServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared-service.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from  '@angular/common/http';
+import{Observable, observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedServiceService {
+
+  readonly apiURL="https://localhost:44362/api";
+  readonly photoURL="https://localhost:44362/photo";
+
+  constructor(private http:HttpClient) { }
+
+  getDepartmentList():Observable<any[]>{
+    return this.http.get<any>(this.apiURL+'/department');
+  }
+
+  addDepartment(dept:any){
+    return this.http.post(this.apiURL+'department',dept);
+  }
+
+  updateDepartment(dept:any){
+    return this.http.put(this.apiURL+'/Department',dept);
+  }
+
+  deleteDepartment(dept:any){
+    return this.http.delete(this.apiURL+'/Department/',dept);
+  }
+
+
+  //Employee APIs
+
+  getEmployeeList():Observable<any[]>{
+    return this.http.get<any>(this.apiURL+'/Employee');
+  }
+
+  addEmployee(dept:any){
+    return this.http.post(this.apiURL+'Employee',dept);
+  }
+
+  updateEmployee(dept:any){
+    return this.http.put(this.apiURL+'/Employee',dept);
+  }
+
+  deleteEmployee(dept:any){
+    return this.http.delete(this.apiURL+'/Employee/',dept);
+  }
+
+  SavePhoto(val:any){
+    return this.http.post(this.apiURL+'/Emmployee/SaveFile',val);
+  }
+
+  getAllDeptNames():Observable<any[]>{
+    return this.http.get<any[]>(this.apiURL+'/Empoyee/GetAllDpt')
+  }
+
+}
